ef9d6297fe37f4316e3108e744ac2ac397637ffc
Lera Holopainen <lera.holopainen@metropolia.fi>
2021-04-01 22:57:28 +0300

    test

--- osa1/tehtava1.1/src/App.js
+++ osa1/tehtava1.1/src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+const Header = (props) => {
+
+}
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
