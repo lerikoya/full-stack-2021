d299bc21f0af9b8b110175aa73ea0060fa62cfb7
Lera Holopainen <lera.holopainen@metropolia.fi>
2021-04-11 16:05:48 +0300

    Tehtävä 1.2:n korjaukset

--- osa1/tehtava1.2/src/App.js
+++ osa1/tehtava1.2/src/App.js
@@ -28,30 +28,37 @@ const Total = (props) => {
   );
 };
 
-const Content = () => {
+const Content = (props) => {
   return (
     <div>
-      <Part part="Fundamentals of React" exercises={10} />
-      <Part part="Using props to pass data" exercises={7} />
-      <Part part="State of a component" exercises={14} />
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   );
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  // const part1 = "Fundamentals of React";
-  // const exercises1 = 10;
-  // const part2 = "Using props to pass data";
-  // const exercises2 = 7;
-  // const part3 = "State of a component";
-  // const exercises3 = 14;
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
       <Header course={course} />
-      <Content />
-      <Total total={10 + 7 + 14} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
