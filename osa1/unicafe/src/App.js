ae30a7b197760f7156b2c483e2aada1233634620
Lera Holopainen <lera.holopainen@metropolia.fi>
2021-06-29 17:16:20 +0300

    tehtävä 1.7 / unicafe step 2

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,23 +1,43 @@
 import React, { useState } from "react";
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [rate, setRate] = useState(0);
+
+  const handleGoodClick = () => {
+    setAll(all + 1);
+    setGood(good + 1);
+    setRate(rate + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setAll(all + 1);
+    setNeutral(neutral + 1);
+    setRate(rate + 0);
+  };
+
+  const handleBadClick = () => {
+    setAll(all + 1);
+    setBad(bad + 1);
+    setRate(rate - 1);
+  };
 
   return (
     <div>
       <div>
         <h1>give feedback</h1>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <button onClick={handleGoodClick}>good</button>
+        <button onClick={handleNeutralClick}>neutral</button>
+        <button onClick={handleBadClick}>bad</button>
       </div>
       <div>
         <h1>statistics</h1>
         <p>
-          good {good} <br></br> neutral {neutral} <br></br> bad {bad}
+          good {good} <br></br> neutral {neutral} <br></br> bad {bad} <br></br>{" "}
+          all {all} <br></br> average {rate / all} <br></br> positive {good * 100 / all}%
         </p>
       </div>
     </div>
