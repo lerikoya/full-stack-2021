bf224cdfb122247d220eab45e3a2c7d0f875bc13
Lera Holopainen <lera.holopainen@metropolia.fi>
2021-06-29 15:47:41 +0300

    tehtävä1.4

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -36,6 +36,7 @@ const Total = (props) => {
 const Content = (props) => {
   return (
     <div>
+
       <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
       <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
       <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
